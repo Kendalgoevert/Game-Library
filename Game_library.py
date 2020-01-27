2e9b8cd264b5ce83b2ee8e50ee21eee42fde3ea8
Kendal Goevert <kgoevert@debian12.CIS.COM>
2020-01-27 12:07:03 -0600

    new file:   Game_library.py

---
+++ Game_library.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+#Kendal Goevert
+#1-27-2020
+
+def add_new():
+    print("running add_new()")
+    
+def print_all_games():
+    print("running print_all_games()")
+    
+def search_by_title():
+    print("running search_by_title()")
+    
+def remove_a_Game():
+    print("running remove_a_game()")
+
+def save_database():
+    print("running save_database()")
+    
+def quit():
+    print("running quit()")
+    
+    
+
+keep_going = True
+
+while keep_going:
+    print("""
+    Welcome to Game Library.
+    ---------------------------
+    
+    MAIN MENU
+    1) Add/Edit Game
+    2) Print All Games
+    3) Search By Title
+    4) Remove a Game
+    5) Save Database
+
+    Q) Quit
+    
+    """)
+    
+    choice = input("What would you like to do? ")
+    if choice == "1":
+        add_new()
+    elif choice == "2":
+        print_all()
+    elif choice == "3":
+        search_by_title()
+    elif choice == "4":
+        remove_a_game
+    elif choice == "5":
+        save_database()
+    elif choice == "Q" or choice == "q":
+        quit()
+        keep_going = False
+    else:
+        print("*** NOT A VALID CHOICE ***\n")
+        
+print("Goodbye.")
+    
